cf033ab12c884460be2753e40ea41d08fbd3413c
Tiago Bohnenberger <tiagoboh10@gmail.com>
2021-06-10 11:46:58 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/tiagobohnenberger/workshopmongo/domain/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.tiagobohnenberger.workshopmongo.domain.resources.exception;
+
+import com.tiagobohnenberger.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/tiagobohnenberger/workshopmongo/services/UserService.java
+++ src/main/java/com/tiagobohnenberger/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.tiagobohnenberger.workshopmongo.services;
 
 import com.tiagobohnenberger.workshopmongo.domain.User;
 import com.tiagobohnenberger.workshopmongo.repository.UserRepository;
+import com.tiagobohnenberger.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
